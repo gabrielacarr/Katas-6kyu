f3470aaa09199e6114db09e02c708d5598e08191
GABRIELA <24256452+gabrielacarr@users.noreply.github.com>
2020-06-08 13:47:31 -0400

    pangram

--- index.js
+++ index.js
@@ -103,3 +103,22 @@ function letterCount(s) {
     {}
   );
 }
+
+
+// https://www.codewars.com/kata/545cedaa9943f7fe7b000048/train/javascript
+function isPangram(string) {
+  //...
+  //   let a = 97;
+  //   let z = 122;
+
+  //   for(let i = a; i <= z; i++) {
+  //     if(!string.toLowerCase().includes(String.fromCharCode(i))) return false;
+  //   }
+  //   return true;
+  //   let regex = /[^a-z]+/gi;
+  //   const worth = string.toLowerCase().replace(regex, '');
+  //   const itsMatched = new Set(Array.from(worth)).size;
+  //   return itsMatched === 26;
+  return [..."abcdefghijklmnopqrstuvwxyz"].every(
+    (char) => string.toLowerCase().indexOf(char) !== -1
+  );
