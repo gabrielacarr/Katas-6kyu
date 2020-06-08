3c3a2cd7df4f8e3dcd48bcdecaecd1b1e6cf017f
GABRIELA <24256452+gabrielacarr@users.noreply.github.com>
2020-06-08 13:47:04 -0400

    reuce

--- index.js
+++ index.js
@@ -80,3 +80,26 @@ var splitInteger = function (num, parts) {
   //     array[i] = Math.floor(num / parts) + (i < num % parts);
   //   return array;
 };
+
+// https://www.codewars.com/kata/5808ff71c7cfa1c6aa00006d/train/javascript
+function letterCount(s) {
+  //your code here
+  //   let hash = {};
+  //   for(let i = 0; i < s.length; i++) {
+  //     if(hash[s[i]]) {
+  //       hash[s[i]]++;
+  //     } else {
+  //       hash[s[i]] = 1;
+  //     }
+  //   }
+  //   return hash;
+  //   let obj = {};
+  //   s.split('').forEach(el => {
+  //     obj[el]? obj[el]++ : obj[el]=1;
+  //   });
+  //   return obj;
+  return [...s].reduce(
+    (acc, val) => (acc[val] ? acc[val]++ : (acc[val] = 1), acc),
+    {}
+  );
+}
