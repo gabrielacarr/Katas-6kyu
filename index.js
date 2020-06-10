6c08dd6171e052cf3ae2a435983a1d7ac6c100fd
GABRIELA <24256452+gabrielacarr@users.noreply.github.com>
2020-06-10 13:58:47 -0400

    zero

--- index.js
+++ index.js
@@ -122,3 +122,28 @@ function isPangram(string) {
   return [..."abcdefghijklmnopqrstuvwxyz"].every(
     (char) => string.toLowerCase().indexOf(char) !== -1
   );
+    
+  // https://www.codewars.com/kata/5d41e16d8bad42002208fe1a/train/javascript
+function moreZeros(s) {
+  let array = s.split("");
+  let newArray = array.map((char) => {
+    return char.charCodeAt(0).toString(2);
+  });
+  let solution = [];
+  for (let i = 0; i < newArray.length; i++) {
+    let zeroCount = 0;
+    let oneCount = 0;
+    for (let j = 0; j < newArray[i].length; j++) {
+      if (newArray[i][j] == 0) {
+        zeroCount++;
+      } else {
+        oneCount++;
+      }
+    }
+    if (oneCount < zeroCount) {
+      solution.push(s[i]);
+    }
+  }
+  return [...new Set(solution)];
+}
+  
