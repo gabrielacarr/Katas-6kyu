af343fd85bcce295eecdfd10b5922cd8433aee26
GABRIELA <24256452+gabrielacarr@users.noreply.github.com>
2020-06-12 13:51:40 -0400

    answer

--- index.js
+++ index.js
@@ -174,3 +174,20 @@ function hungrySeven(arr) {
 //   }
 //   return arr.split('').map(x => x*1);
 // https://www.codewars.com/kata/525c65e51bf619685c000059/train/javascript
+    
+ function cakes(recipe, available) {
+  let ans = Infinity;
+  for (let key in recipe) {
+    if (available[key]) {
+      if (ans > available[key] / recipe[key]) {
+        ans = Math.floor(available[key] / recipe[key]);
+      }
+    } else {
+      ans = 0;
+      break;
+    }
+  }
+  return ans;
+
+  //   return Math.min(...Object.keys(recipe).map(key => Math.floor(available[key]/recipe[key] || 0)))
+}
